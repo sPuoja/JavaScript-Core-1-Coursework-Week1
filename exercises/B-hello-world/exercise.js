c3faa4701403a0d2efdb714d3b40f6d228b3dfde
sPuoja <84133321+sPuoja@users.noreply.github.com>
2021-07-02 12:57:28 +0100

    Done exercise B

--- exercises/B-hello-world/exercise.js
+++ exercises/B-hello-world/exercise.js
@@ -1,1 +1,11 @@
+
+
 console.log("Hello world");
+
+console.log("Hello, I'm still learning the tricks in VS code.");
+
+console.log("Change directory is quite confusing!");
+
+console.log(This is easy now!);
+
+console.log(9);
